ce9174a53befad1cd821af86f5d09d9e66e1df57
iamaditya10 <aditya.bharti@fetch.ai>
2021-06-30 21:55:34 +0530

    get current staker before finalize added

--- migrations/4_deploy_simpleStakingPool.js
+++
@@ -1,7 +0,0 @@
-let simpleStakePool = artifacts.require("simpleStakePool");
-let ERC20Token = artifacts.require("CrowdsaleToken");
-let Auction = artifacts.require("dutchStaking");
-
-module.exports = function(deployer) {
-  deployer.deploy(simpleStakePool, ERC20Token.address, Auction.address);
-};
